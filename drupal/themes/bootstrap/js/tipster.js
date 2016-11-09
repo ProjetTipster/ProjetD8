933e364620b52865800ca95ce88a1aa2a07a676c
nico <nicolas.h75020@gmail.com>
2016-11-09 17:25:15 +0100

    crea module tipster

--- drupal/themes/bootstrap/js/tipster.js
+++ drupal/themes/bootstrap/js/tipster.js
@@ -1,4 +1,15 @@
 (function($, Drupal, drupalSettings){
     $('#edit-entity-follower').hide('true');
-    $('#edit-entity-tipster').show('true');
+    $('#edit-entity-tipster').hide('true');
+
+    $('#edit-profile-utilisateur-follower').click(function () {
+        $('#edit-entity-follower').show('true');
+        $('#edit-entity-tipster').hide('true');
+    });
+
+    $('#edit-profile-utilisateur-tipster').click(function () {
+        $('#edit-entity-follower').hide('true');
+        $('#edit-entity-tipster').show('true');
+    });
+
 })(jQuery, Drupal, drupalSettings);
